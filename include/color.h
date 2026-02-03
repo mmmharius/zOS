4b9c0f314c000aa4b22d78dc78dec7871bd8f445
Marius <mariuspapin@rcmm.fr>
2026-02-03 18:33:50 +0100

    start implemanting color defined use

---
+++ include/color.h
@@ -0,0 +1,25 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define BLACK           0x0
+#define BLUE            0x1
+#define GREEN           0x2
+#define CYAN            0x3
+#define RED             0x4
+#define MAGENTA         0x5
+#define BROWN           0x6
+#define L_GRAY          0x7
+#define D_GRAY          0x8
+#define L_BLUE          0x9
+#define L_GREEN         0xA
+#define L_CYAN          0xB
+#define L_RED           0xC
+#define L_MAGENTA       0xD
+#define YELLOW          0xE
+#define WHITE           0xF
+
+#define VGA_COLOR(fg, bg) ((bg << 12) | (fg << 8))
+
+#define VGA_DEFAULT_COLOR VGA_COLOR(WHITE, BLACK)
+
+#endif
--- include/screen.h
+++ include/screen.h
@@ -1,7 +1,6 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#define VGA_COLOR 0x0F00
 #define VGA_ADDR 0xB8000
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
