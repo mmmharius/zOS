acbb9eeca0c82e1dd9f5b57d6b248e793b0f95b5
Marius <mariuspapin@rcmm.fr>
2026-02-05 02:49:24 +0100

    Makefile change : make debug, add debug features

---
+++ include/debug.h
@@ -0,0 +1,14 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <color.h>
+#include <keyboard.h>
+#include <screen.h>
+#include <stdint.h>
+#include <io.h>
+#include <printk.h>
+
+void    print_screen_struct();
+void    print_current_struct();
+
+#endif
